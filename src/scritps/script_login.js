4ce58832f00d439fa9403803ff5a4ceb8efec7b5
Igson <igson660@gmail.com>
2022-10-16 11:42:49 -0500

    :bug: layout adjustments

--- src/scritps/script_login.js
+++ src/scritps/script_login.js
@@ -1,8 +1,10 @@
 import * as utils from "./utils.js";
 const HALF_SECOND = 500;
 
+// verify data and redirect
+utils
+  .$selector(".button-form")
+  .addEventListener("click", () => utils.toLogin());
+
 //trasition icon
 setInterval(() => utils.varifyContentInput(), HALF_SECOND);
-
-// verify data and redirect
-utils.$selector(".button-form").addEventListener("click", () => utils.toLogin());
